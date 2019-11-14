776c6c44ea992fc932758c4c9996022c6b894fc6
Aidan Mooney <amooney034@gmail.com>
2019-11-14 09:55:45 -0500

    helo

--- src/sum.js
+++ src/sum.js
@@ -2,8 +2,14 @@ const readlineSync = require("readline-sync");
 
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
+let lowerBound =Number(readlineSync.question("\nLower bound: "));
+let upperBound = Number(readlineSync.question("Upper bound: "));
 
-let lowerBound = -1;
-while (lowerBound < 0 || lowerBound > 100) {
-    lowerBound = readlineSync.question("\nLower bound: ");
+while ((upperBound < MIN) || (lowerBound > upperBound) || (lowerBound < MIN) || (lowerBound > MAX) || (upperBound > MAX) || (!(Number.isInteger(upperBound)))
+|| (!(Number.isInteger(lowerBound))) || (Number.isNaN(lowerBound)) || (Number.isNaN(upperBound)))  {
+  lowerBound = Number(readlineSync.question("Lower bound: "));
+  upperBound = Number(readlineSync.question("Upper bound: "));
 }
+
+final = 
+console.log("\n" + final + ".\n");
