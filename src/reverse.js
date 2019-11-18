a799c5d9594dd40b76db26386a448263ae7faca7
Aidan Mooney <amooney034@gmail.com>
2019-11-18 09:31:40 -0500

    Jets

---
+++ src/reverse.js
@@ -0,0 +1,21 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = Number.MAX_SAFE_INTEGER;
+let input = Number(readlineSync.question("\nPositive integer: "));
+
+while ((input < MIN) || (input > MAX) || (!(Number.isInteger(input))) ||
+(Number.isNaN(input)))  {
+  input = Number(readlineSync.question("Positive integer: "));
+}
+
+function reverseNumber(input) {
+  var revNumber = 0;
+  while (input > 0) {
+    revNumber = (revNumber * 10) + (input % 10);
+    input = Math.floor(input / 10);
+  }
+  return revNumber;
+}
+
+console.log(reverseNumber(input));
