98e9d0475f6a768f2ffe0177f796f71155c37b03
Aidan Mooney <amooney034@gmail.com>
2019-11-20 10:12:36 -0500

    finally fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -3,10 +3,18 @@ const readlineSync = require("readline-sync");
 const MIN = 1;
 const MAX = 78;
 let input = Number(readlineSync.question("\nPositive integer: "));
+let sqrtFive = Math.sqrt(5);
+let phi = ((1 + sqrtFive) / 2);
 
-while ((input < MIN) || (input > MAX) || (!(Number.isInteger(input))) ||
-(Number.isNaN(input)))  {
+
+while ((Number.isNaN(input)) || (input < MIN) || (input > MAX) ||
+(!(Number.isInteger(input))))  {
   input = Number(readlineSync.question("Positive integer: "));
 }
 
-let first = input + 1
+let primary = ((phi ** input) - ((-phi) ** -input));
+let secondary = (primary / sqrtFive);
+let final = Math.round(secondary);
+let finished = final.toLocaleString('en');
+
+console.log("\n" + finished + ".\n");
